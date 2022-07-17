068e4deb7964fc4984508d2fdd9b49ffb7bf5a7d
ShubzzRaut <rautshubham815@gmail.com>
2022-07-16 21:16:49 -0700

    updated

--- welcome.java
+++ welcome.java
@@ -1,1 +1,11 @@
-welcome to git
+class Program 
+{
+   public static void main (String args[])
+   {
+       System.out.println("welcome to the git world");
+      System.out.println("jai");
+      System.out.println("shree");
+      System.out.println("Ram");
+    }
+
+}
